626481fad5264709511d1528c5165954b84904f9
Mradul <94035599+21BEC@users.noreply.github.com>
2023-06-22 19:47:38 +0530

    Create conversion.sol

---
+++ conversion.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+contract conversion {
+    //function to convert ether into other forms of it
+    function Convert() public payable returns(uint256 ethervalue, uint256 weivalue, uint256 gweivalue)  {
+        ethervalue = msg.value;
+        weivalue = msg.value;
+        gweivalue = msg.value;
+    }
+}
