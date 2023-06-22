c806a19a650c059a2a2d43f4337a127a02b9c908
Mradul <94035599+21BEC@users.noreply.github.com>
2023-06-22 19:50:02 +0530

    Update conversion.sol

--- conversion.sol
+++ conversion.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.9;
 contract conversion {
     //function to convert ether into other forms of it
     function Convert() public payable returns(uint256 ethervalue, uint256 weivalue, uint256 gweivalue)  {
+//payable function means that it contracts and take value from the user for conversion
         ethervalue = msg.value;
         weivalue = msg.value;
         gweivalue = msg.value;
